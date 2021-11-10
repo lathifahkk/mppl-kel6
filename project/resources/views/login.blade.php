e86d5f238e9dd2dfae0a83dd7f99e3eae784df11
anggario21d <anggario_21@apps.ipb.ac.id>
2021-11-10 14:51:40 +0700

    login done

---
+++ project/resources/views/login.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pecel Mba Merr</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <div class="header-logo">Lo<span>go</span></div>
+                    <div class="header-list">
+                        <ul>
+                            <li>Home</li>
+                            <li>Product</li>
+                        </ul>
+                    </div>
+            </div>
+        </div>
+    
+        <div class="container">
+            <div class="main">
+                <div class="login">
+                    <img src="img/pic-1.png" alt="login pic">
+                </div>
+                <div class="login">
+                    <h1>Belanja Sambal Pecal Nganjuk,</h1>
+                    <h1>menjadi lebih mudah</h1>
+                    <p>Email Address</p>
+                    <input>
+                    <p>Password</p>
+                    <input>
+                    <a href="#" class="btn sign-in">Sign in to My Account</a>
+                    <a href="#" class="btn sign-up">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    
+        <div class="footer">
+          <p>2021 Copyright Store. All Rights Reserved.</p>
+        </div>
+      </body>
+</html>
